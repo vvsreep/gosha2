3ca83417b4df8ce527c5f4e896540d3bd52c7ebd
vvs <aa151972@gmail.com>
2017-12-04 19:04:44 +0300

    Начало начал4

--- main.py
+++ main.py
@@ -1,6 +1,55 @@
-#!/usr/bin/python3
+# for j in 'hello word':
+#     # print(j * 2)
+#     if j == 'a':
+#         break
+#     print(j * 2)
+# else:
+#     print('буквы \'a\' нету в словах')
+#
+#         # print(j*2, end='')
+#
+
+# i = 0
+# while i <= 10:
+#     print(i)
+#     i += 3
+
+# !/usr/bin/python3
 # -*- coding: utf-8 -*-
 # test = 'String'
 # # print('равно \n', (29 // 6))
 # print('\ntest=', test)
+# num1 = int(input("Enter first num:"))
+# num2 = int(input("Enter second num:"))
+# res = num1 + num2
+# print("Result is", res)
+# n = int(input('введи число \n'))
+# if n == 0:
+#     print('равно 0')
+# # elif n > 0:
+# #     print('больше 0')
+# # elif n < 0:
+# #     print('меньше 0')
+# else:
+#     print('хуй!!!')
+# # print('заебись')
+l = []
+l.append(23)
+l.append(55)
+l.append(78)
+l.append(1)
+b=[44,90,55]
+# b=['sav','wefwef',55]
+l.extend(b)
+l.insert(1,6666)
+# l.remove(55)
+# l.pop()
+# ll=l.index(55)
+# print(ll)
+# l3=l.count(55)
+# l3=l.sort()
+
+print(l)
+l.sort()
+print(l)
 

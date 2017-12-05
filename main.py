48d7c157df3d69dfaed2e9698bf4a8bebb1829db
vvs <aa151972@gmail.com>
2017-12-05 12:37:43 +0300

    Начало начал4

--- main.py
+++ main.py
@@ -33,23 +33,53 @@
 # else:
 #     print('хуй!!!')
 # # print('заебись')
-l = []
-l.append(23)
-l.append(55)
-l.append(78)
-l.append(1)
-b=[44,90,55]
-# b=['sav','wefwef',55]
-l.extend(b)
-l.insert(1,6666)
-# l.remove(55)
-# l.pop()
-# ll=l.index(55)
-# print(ll)
-# l3=l.count(55)
-# l3=l.sort()
+# l = []
+# l.append(23)
+# l.append(55)
+# l.append(78)
+# l.append(1)
+# b=[44,90,55]
+# # b=['sav','wefwef',55]
+# l.extend(b)
+# l.insert(1,6666)
+# # l.remove(55)
+# # l.pop()
+# # ll=l.index(55)
+# # print(ll)
+# # l3=l.count(55)
+# # l3=l.sort()
+#
+# print(l)
+# l.sort()
+# print(l)
+# l = [34, 'cd', 56, 34.34]
+# # print(l[-1])
+# # i = 3
+# # # print(i)
+# # # print(l[i])
+# # print('\n')
+# # while i>=0:
+# #     # print(i)
+# #     # print('555')
+# #     print(l[i])
+# #     i -= 1
+# print(l[1:3)
+# a=tuple('hello word')
+# d = {'test': 108, 'te': 44}
+# print(d['te'])
+# d = dict(s='dict', longer='rgewrg ')
+# d = dict([(23, 56), (45, 89)])
+# d['s'] = 235
+# d=dict.fromkeys(['a','b','r'],6)
+# d = {a: a for a in range(7)}
+# a = {i**2 for i in range(10)}
+# a = frozenset
+# print(type(a))
+# print('')
 
-print(l)
-l.sort()
-print(l)
+# print(a)
+def func(x):
+    return x
+print(func(23))
+
 
